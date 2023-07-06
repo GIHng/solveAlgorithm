c41710e9fb64828592e897fceb5d1bbf748f9125
CDoGH <sa03048@gmail.com>
2023-07-06 17:22:44 +0900

    Apply gitignore

--- Python/LV6_Advanced/25206.py
+++ Python/LV6_Advanced/25206.py
@@ -5,13 +5,14 @@ gradeScore = ['4.5', '4.0', '3.5', '3.0', '2.5', '2.0', '1.5', '1.0', '0', '0']
 totalScore = 0
 totalNum = 0
 
-L = []
-for i in range(0, 20):
-    L.append(input().split())
-    if L[i][2] in 'PF'.split():
-        continue
 for j in range(0, 20):
-    totalScore = totalScore + float(L[j][1]) * float(gradeScore[grade.index(L[j][2])])
-    totalNum += float(L[j][1])
-
-print(f"totalScore : ${totalScore} totalNum : ${totalNum} result : ${totalScore/totalNum}, totalNum, totalScore/totalNum")
+    s, p, g = input().split()
+    if g in 'P':
+        continue
+    totalScore += float(p) * float(gradeScore[grade.index(g)])
+    totalNum += float(p)
+if totalScore == 0:
+    print('%.6f' % 0)
+else:
+    result = totalScore/totalNum
+    print('%.6f' % result)
