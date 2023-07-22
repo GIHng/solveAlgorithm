1d7166b4d43f1f68549abcc8a8188c3262d9902a
CDoGH <sa03048@gmail.com>
2023-07-22 22:26:00 +0900

    [SILVER V] Title: 분수찾기, Time: 52ms, Memory: 31256KB - BaekjoonHub

--- Python/LV8_mathematic/1193.py
+++ Python/LV8_mathematic/1193.py
@@ -5,5 +5,7 @@ while N > line:
     N -= line
     line += 1
 
-print(line) 
-
+if (line % 2) == 1:  # 홀수 줄이라면, <---
+    print(line - N + 1, '/', N, sep='')
+elif (line % 2) == 0:
+    print(N, '/', line - N + 1, sep='')
